6591f5a701f822c4b651e1f71d076b64219fdafd
andy <andy@virtualpowersystems.com>
2018-07-10 17:05:20 -0700

    fixing first positive fast

--- andylou2/07_10_2018.py
+++ andylou2/07_10_2018.py
@@ -19,20 +19,46 @@ def findMinMissing(arr):
 			return i
 		found = False
 
+def swap(a, b):
+	temp = a
+	a = b
+	b = a
+	return
 
 def findMinMissingFast(arr):
-	max = 1
+	i = 0
+	while i < len(arr):
+		# print(i, arr[i], arr[arr[i]-1], arr)
+		if arr[i] > 0 and arr[i] < len(arr):
+			temp = arr[i]
+			arr[i], arr[temp-1] = arr[temp - 1], arr[i]
+			if i > 0 and arr[i] > 0 and arr[i] - 1 < len(arr) and arr[i] != arr[arr[i]-1]:
+				i-=1
+			# i-=1
+		i+=1
+
+
+		# input[i] >= 0 and input[i] < len(input) and input[i] != i and input[input[i]] != input[i]:
+
 	for i in range(len(arr)):
-		if arr[i] >= 0 and arr[i] <= max:
-			max = arr[i] + 1
-	return max
+		if arr[i] != i + 1:
+			return i + 1
+	return len(arr) + 1
 
 
 def main():
 	# arr = [1,5,7,-1,2,4,5,5]
-	# arr = [0]
-	arr = [-1, 1,2,-3,4,5,8]
+	# arr = [2, 1]
+	# arr = [-1, 1,2,-3,4,5,8]
+	# arr = [-10,-3,-100,-1000,-239,1]
+	# arr = [1,2,3,4,5,6,7,8]
+	# arr = [-1,4,2,1,9,10]
+	# arr = [11,1,6,11,5,5,-6,9,-3,9,5,4,2,-8,16,-6,-4,2,3]
+	# arr = [3,5,-3,-10,3,-4,9,7,-3,-1]
+	# arr = [39,8,43,12,38,11,-9,12,34,20,44,32,10,22,38,9,45,26,-4,2,1,3,3,20,38,17,20,25,41,35,37,18,37,34,24,29,39,9,36,28,23,18,-2,28,34,30]
+	arr = [-6,2,-9,5,3,12,-1,0,11,-4,18,1,-4,-8,9,-4,2,1,-6,1]
 
+	print(arr)
 	m = findMinMissingFast(arr)
 	print(arr)
 	print("The first missing positive integer is: {}".format(m))
