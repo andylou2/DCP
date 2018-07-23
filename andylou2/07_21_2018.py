3baa9ec1e1e70618fce98af461d9cc1b6d0f0ac9
andy <andy@virtualpowersystems.com>
2018-07-23 09:10:31 -0700

    random number from stream

--- andylou2/07_21_2018.py
+++ andylou2/07_21_2018.py
@@ -1,4 +1,28 @@
 # This problem was asked by Facebook.
 
 # Given a stream of elements too large to store in memory, pick a random element from the stream with uniform probability.
+import random
+
+def rand_stream():
+	num = 0
+	count = 0
+	while True:
+		i = raw_input()
+		if str(i) == 'q':
+			break
+		i = int(i)
+		if count == 0:
+			num = i
+		else:
+			r = random.randint(0, count)
+			if r == count:
+				num = i
+		count += 1
+	print("your number was: {}".format(num))
+
+def main():
+	rand_stream()
+
+if __name__ == '__main__':
+	main()
 
