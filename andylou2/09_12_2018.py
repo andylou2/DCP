1747424248e10446f4fd4faf38aaf0fbffebab85
andy <andy@virtualpowersystems.com>
2018-09-13 10:46:19 -0700

    N bishops

--- andylou2/09_12_2018.py
+++ andylou2/09_12_2018.py
@@ -21,3 +21,76 @@
 # [0 0 0 0 0]
 # [b 0 0 0 0]
 # You should return 2, since bishops 1 and 3 attack each other, as well as bishops 3 and 4.
+def nCr(n, r):
+	numerator = reduce(lambda x, y : x * y, range(n - r + 1, n + 1))
+	denominator = reduce(lambda x, y : x * y, range(1, r + 1))
+	res = numerator / (denominator + 0.0)
+	return res
+
+
+def numAttacks(arr, M):
+	bishops = {}
+	for loc in arr:
+		bishops[loc] = False
+	totalPairs = 0
+	for loc in arr:
+		if bishops[loc] == True:
+			pass
+		else:
+			numBishop = getBishopsDiag(loc, bishops, M)
+			numPairs = nCr(numBishop, 2)
+			totalPairs += numPairs
+	return int(totalPairs)
+
+def getBishopsDiag(loc, bishops, M):
+	x = loc[0]
+	y = loc[1]
+
+	numBishop = 1
+	while x < M and y < M:
+		x += 1
+		y += 1
+		curr = (x,y)
+		if curr in bishops:
+			bishops[curr] = True
+			numBishop += 1
+
+	x = loc[0]
+	y = loc[1]
+	while x >= 0 and y >= 0:
+		x -= 1
+		y -= 1
+		curr = (x,y)
+		if curr in bishops:
+			bishops[curr] = True
+			numBishop += 1
+
+	x = loc[0]
+	y = loc[1]
+	while x < M and y >= 0:
+		x += 1
+		y -= 1
+		curr = (x,y)
+		if curr in bishops:
+			bishops[curr] = True
+			numBishop += 1
+
+	x = loc[0]
+	y = loc[1]
+	while x >= 0 and y < M:
+		x -= 1
+		y += 1
+		curr = (x,y)
+		if curr in bishops:
+			bishops[curr] = True
+			numBishop += 1
+	return numBishop
+
+def main():
+	arr = [(0, 0), (1, 2), (2, 2), (4, 0), (1, 1)]
+	arr = [(0, 0), (1, 1), (2, 0), (0,2)]
+	M = 5
+	print(numAttacks(arr, M))
+
+if __name__ == '__main__':
+	main()
